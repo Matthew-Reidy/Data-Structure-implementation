57300096ecf069c518510f967e722dadef613b59
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-10-04 14:17:48 -0700

    Create frontend.java

---
+++ 2021ReWrite/frontend.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Frontend {
+    public void UserInput ( ){
+        System.out.println("enter your menu selection");
+        Scanner scan = new Scanner(System.in);
+        int selection = scan.nextInt();
+    }
+
+    public void ActionMenu (int selection){
+        switch (selection){
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+        }
+    }
+}
