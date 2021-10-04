107bec1bedd8c67931e9c2e24178d414d02e4892
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-10-04 14:21:27 -0700

    Create Backend.java

---
+++ 2021ReWrite/Backend.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Backend {
+    HashMap<String, Integer> Stock = new HashMap<>();
+    
+    public void Add (HashMap<String, Integer> Stock )
+    {
+        
+    }
+    public void Remove(HashMap<String, Integer> Stock)
+    {
+        
+    }
+    public static int search(HashMap<String, Integer> Stock )
+    {
+        
+        return 0;
+    }
+    
+}
+
