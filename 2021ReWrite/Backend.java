816543a3ef840bcc5074b1beac527c5a5691c56d
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-10-04 15:25:40 -0700

    Update Backend.java

--- 2021ReWrite/Backend.java
+++ 2021ReWrite/Backend.java
@@ -2,20 +2,34 @@ import java.util.HashMap;
 
 public class Backend {
     HashMap<String, Integer> Stock = new HashMap<>();
-    
-    public void Add (HashMap<String, Integer> Stock )
+
+    public void Add (HashMap<String, Integer> Stock, String key, int val )
     {
-        
+      if( Stock.containsKey(key)){
+          System.out.println("This item already exists");
+          ModifyItem(Stock, key);
+      }else {
+          Stock.put(key, val);
+      }
+    }
+    public void ModifyItem(HashMap<String, Integer> Stock, String key )
+    {
+
     }
     public void Remove(HashMap<String, Integer> Stock)
     {
-        
+
     }
-    public static int search(HashMap<String, Integer> Stock )
+    public static int search(HashMap<String, Integer> Stock, String key )
     {
-        
+
+        if(!Stock.containsKey(key)){
+
+        }else{
+
+        }
+
         return 0;
     }
-    
-}
 
+}
