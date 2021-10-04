ca18cc3e1b148523711d26b8c1d4b1c444995262
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-10-04 15:25:16 -0700

    Update frontend.java

--- 2021ReWrite/frontend.java
+++ 2021ReWrite/frontend.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+
 public class Frontend {
     public void UserInput ( ){
+
         System.out.println("enter your menu selection");
         Scanner scan = new Scanner(System.in);
         int selection = scan.nextInt();
@@ -8,14 +10,18 @@ public class Frontend {
 
     public void ActionMenu (int selection){
         switch (selection){
-            case 1:
-
-            case 2:
-
-            case 3:
-
-            case 4:
-
+            case 1: //add
+                Backend add = new Backend();
+                add.add();
+            case 2: //remove
+                Backend remove = new Backend();
+                remove.remove();
+            case 3: //search
+                Backend search  = new Backend();
+                search.search;
+            case 4: //modify
+                Backend modify  = new Backend();
+                modify.ModifyItem;
         }
     }
 }
